d334faf8e60c364947a1df41ebc7453b908e3961
seperman.jh <seperman.jh@gmail.com>
2021-09-20 00:29:05 -0700

    1) 쿼리 type 셋업 (기초) -- scheme 2) 쿼리 데이터 셋업 (기초) -- resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,21 @@
+const user_result = {
+    user_id         : "jhlee",
+    name            : "Jonghwa Lee",
+    email           : "seperman.jh@gmail.com",
+    lastlogin_date  : "2021-09-01 09:00:00",
+    age             : "34",
+    gender          : "male"
+};
+
+
 const resolvers = {
     Query:{
-        name : () => "seperman.jh"
+        user: () => user_result
     }
 };
+
+
+
+
 
 export default resolvers;
